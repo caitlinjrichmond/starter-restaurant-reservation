9eec8a660374157e3f02db75fb23cafe24e2a31d
Caitlin <caitlinjrichmond@gmail.com>
2022-04-02 17:24:31 -0600

    US 4 Front End Tests Pass

--- front-end/src/tables/SeatTable.js
+++ front-end/src/tables/SeatTable.js
@@ -3,11 +3,12 @@ import { useParams, useHistory } from "react-router-dom";
 import { getTablesList, seatTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 
+
 function SeatTable() {
   const [tables, setTables] = useState([]);
   const [tablesError, setTablesError] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
-  const [seating, setSeating] = useState("")
+  const [seating, setSeating] = useState("");
   const reservation_id = useParams().reservation_id;
   const history = useHistory();
   // const [data, setData] = useState({})
@@ -25,16 +26,15 @@ function SeatTable() {
   //   return () => abortController.abort();
   // }
 
-
   useEffect(() => {
     setTables([]);
     const abortController = new AbortController();
 
     async function loadTables() {
-      try{
+      try {
         const response = await getTablesList(abortController.signal);
         setTables(response);
-      } catch(error) {
+      } catch (error) {
         if (error.name === "AbortError") {
           console.log("Aborted");
         } else {
@@ -48,14 +48,14 @@ function SeatTable() {
       console.log("clean up");
       abortController.abort();
     };
-  }, [reservation_id])
+  }, [reservation_id]);
 
   const handleChange = (event) => {
-    event.preventDefault()
-    setSeating(event.target.value)
-  }
+    event.preventDefault();
+    setSeating(event.target.value);
+  };
 
-  console.log(seating, reservation_id)
+  console.log(seating, reservation_id);
 
   // const handleSubmit = (event) => {
   //   event.preventDefault();
@@ -74,44 +74,47 @@ function SeatTable() {
   const handleSubmit = (event) => {
     event.preventDefault();
     seatTable(seating, reservation_id)
-    .then(() => {
-      setSeating("");
-      history.push("/")
-    })
-    .catch((error) => {
-      setErrorMessage(error)
-    })
-
-  }
-
+      .then(() => {
+        setSeating("");
+        history.push("/");
+      })
+      .catch((error) => {
+        setErrorMessage(error);
+      });
+  };
 
   return (
     <>
       <h1>Seat A Reservation</h1>
       <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="table_id">
-          Select a Table for Reservation #{reservation_id}:
-          <br />
-          <select value={seating} name="table_id" onChange={handleChange}>
-             <option value="0">Table Options</option>
+        <div className="form-group">
+          <label htmlFor="table_id">
+            Select a Table for Reservation #{reservation_id}:
+            <br />
+            <select value={seating} name="table_id" onChange={handleChange}>
+              <option key="0">--</option>
+    
               {tables.map((table) => {
-                  return (<option key={table.table_id} value={table.table_id}>{table.table_name} - {table.capacity} </option>)
+                return (          
+                  <option key={table.table_id} value={table.table_id}>
+                    {table.table_name} - {table.capacity}
+                  </option>
+                );
               })}
-          </select>
-        </label>
-        <br />
-        <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => history.goBack()}
-            >
-              Cancel
-            </button>{" "}
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-      </div>
+            </select>
+          </label>
+          <br />
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => history.goBack()}
+          >
+            Cancel
+          </button>{" "}
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
       </form>
       <>{errorMessage ? <ErrorAlert error={errorMessage} /> : null}</>
     </>
