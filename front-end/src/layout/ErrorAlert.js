0ef12812df8c5ea2071f30cbd4ec55b9024356f7
Caitlin Richmond <89586901+caitlinjrichmond@users.noreply.github.com>
2022-04-07 17:22:45 -0600

    Update ErrorAlert.js
    cleaned up

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -11,9 +11,7 @@ import React from "react";
 function ErrorAlert({ error }) {
   return (
     error && (
-      <div className="alert alert-danger m-2">
-        Error: {error.response.data.error}
-      </div>
+      <div className="alert-danger">Error: {error.response.data.error}</div>
     )
   );
 }
