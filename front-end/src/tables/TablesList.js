de01ff37d16a79e47450251e605aa3d4a9f7008e
Caitlin Richmond <89586901+caitlinjrichmond@users.noreply.github.com>
2022-04-07 19:33:26 -0600

    Update TablesList.js
    clean up

--- front-end/src/tables/TablesList.js
+++ front-end/src/tables/TablesList.js
@@ -1,44 +1,15 @@
 import React from "react";
-import FinishSeat from "./FinishSeat";
+import TableCard from "./TableCard";
 
 //Function that takes in data from tables table and maps tables into individual squares (grid style)//
 
 function TablesList({ tables }) {
   return (
     <div className="container">
-      <div className="row">
-        {tables ? (
-          tables.map((table) => (
-            <div
-              className="card"
-              style={{
-                width: "10rem",
-                marginBottom: "20px",
-                marginRight: "10px",
-              }}
-              key={table.table_id}
-            >
-              <div className="card-body table-card">
-                {table.table_name} <br /> Seats {table.capacity} <br />
-                {!table.reservation_id ? (
-                  <span data-table-id-status={table.table_id}>Free</span>
-                ) : (
-                  <>
-                    <span data-table-id-status={table.table_id}>Occupied</span>
-                    <br />
-                    <FinishSeat table={table} />
-                  </>
-                )}
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
-        <br />
-      </div>
+      {tables ? <TableCard tables={tables} /> : <p>Loading...</p>}
     </div>
   );
 }
 
 export default TablesList;
+
